87df6898bb7e135cd793bb3b39e0ef7d215c1733
karosaxy <araromirichard@gmail.com>
2024-01-17 00:26:50 +0100

    connected a db, added a model for the snipets

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	
+	_ "github.com/go-sql-driver/mysql"
+	
+	"github.com/araromirichard/snippetcase/pkg/models/mysql"
 )
 
 // Define an application struct to hold the application-wide dependencies for the
@@ -12,6 +17,7 @@ import (
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 // use the http.NewServerMux() function to initiallize a new server
@@ -20,6 +26,9 @@ func main() {
 
 	// define a new command line flag named "port" and a default value of ":4000"
 	port := flag.String("addr", ":4000", "port to listen on")
+	// Define a new command-line flag for the MySQL DSN string.
+
+	dsn := flag.String("dsn", "web:localhost01@/snippetcase?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
@@ -28,9 +37,19 @@ func main() {
 
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
+
 	app := &application{
 		errorLog: errLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
@@ -47,9 +66,21 @@ func main() {
 
 	infoLog.Printf("Starting server on port %s", *port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	errLog.Fatal(err)
 
 	// go run ./cmd/web >>/tmp/info.log 2>>/tmp/error.log will help to direct the logs to an on-disk file
+}
+
+// openDB opens a new database connection. wraps the database/sql.Open() function and returns a sql.DB connection pool for a given DSN
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/radovskyb/watcher v1.0.7 // indirect
